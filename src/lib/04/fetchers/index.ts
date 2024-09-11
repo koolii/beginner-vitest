f63c7b313590770d08118f4d8a4c5d4c45687ef2
koolii <koolii0909@gmail.com>
2024-09-11 15:10:15 +0900

    feat: impl dummy http fetchers

--- src/lib/04/fetchers/index.ts
+++ src/lib/04/fetchers/index.ts
@@ -1,15 +1,23 @@
-import { HttpError, Profile } from "./type";
+import { Articles, HttpError, Profile } from "./type";
 
 export const httpError: HttpError = {
   err: { message: "internal server error" },
 };
 
+async function handleResponse(res: Response) {
+  const data = await res.json();
+  if (!res.ok) {
+    throw data;
+  }
+  return data;
+}
+
+const host = (path: string) => `https://myapi.testing.com${path}`;
+
 export function getMyProfile(): Promise<Profile> {
-  return fetch("https://myapi.testing.com/my/profile").then(async (res) => {
-    const data = await res.json();
-    if (!res.ok) {
-      throw data;
-    }
-    return data;
-  });
+  return fetch(host("/my/profile")).then(handleResponse);
+}
+
+export function getMyArticles(): Promise<Articles> {
+  return fetch(host("/my/articles")).then(handleResponse);
 }
